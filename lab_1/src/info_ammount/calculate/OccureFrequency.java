2ad846eef98a5b2413f482d3b0a6c9cf2218c54a
O. V. Siroklyn <o.v.siroklyn@student.csn.khai.edu>
2023-09-11 21:03:37 +0300

    OccureFrequency class finished

--- lab_1/src/info_ammount/calculate/OccureFrequency.java
+++ lab_1/src/info_ammount/calculate/OccureFrequency.java
@@ -1,14 +1,27 @@
 package info_ammount.calculate;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class OccureFrequency {
-    public static double charFrequency(String word, char n) {
+    public static double charFrequency(String word, char n) { // Здійснює підрахунок частоти появи символу в тексті повідомлення
         int freq = 0;
         for (int i = 0; i < word.length(); i++) {
             if (word.charAt(i) == n){
                 freq++;
             }
         }
-        return Double.valueOf(freq)/Double.valueOf(word.trim().length());
+        return (double) freq / (double) word.length();
     }
-
+    public static HashMap<Character, Double> allCharCounter(String word){ // Видає перелік усіх частот появи у форматі HashSet
+        LinkedHashMap<Character, Double> allChar = new LinkedHashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            allChar.put(word.charAt(i), 0.0);
+        }
+        for (Character i : allChar.keySet()){
+            allChar.put(i, charFrequency(word, i));
+        }
+        System.out.println(allChar);
+        return allChar;
+    }
 }
