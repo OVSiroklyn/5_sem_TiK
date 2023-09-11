7e96c5269114e4ea94a26319e31af3ca76211bc5
1wwg2 <105918939+1wwg2@users.noreply.github.com>
2023-09-11 18:47:10 +0300

    sol

--- lab_1/src/info_ammount/Main.java
+++ lab_1/src/info_ammount/Main.java
@@ -1,9 +1,12 @@
 package info_ammount;
 
+import info_ammount.calculate.InfoAmmount;
 import info_ammount.gui.GUI;
 
 public class Main {
     public static void main(String[] args) {
+        InfoAmmount b1 = new InfoAmmount(200, 2000, 33);
+       System.out.println(b1.MethodHartli());
         GUI gui = new GUI();
         gui.setVisible(true);
     }
