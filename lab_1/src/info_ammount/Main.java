2e3dfc9ec2c6922a036ec21e3ff202605f3b8d0a
O. V. Siroklyn <108236230+OVSiroklyn@users.noreply.github.com>
2023-09-11 21:58:27 +0300

    Update Main.java

--- lab_1/src/info_ammount/Main.java
+++ lab_1/src/info_ammount/Main.java
@@ -5,7 +5,6 @@ import info_ammount.gui.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(OccureFrequency.charFrequency("dasdasdaaaas", 'a'));
         GUI gui = new GUI();
         gui.setVisible(true);
     }
