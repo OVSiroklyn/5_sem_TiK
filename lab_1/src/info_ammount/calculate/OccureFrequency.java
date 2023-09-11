6c5e9c576154ded6158be44b58f8add7d091fb5c
O. V. Siroklyn <o.v.siroklyn@student.csn.khai.edu>
2023-09-11 18:45:55 +0300

    OccureFrequency method added

--- lab_1/src/info_ammount/calculate/OccureFrequency.java
+++ lab_1/src/info_ammount/calculate/OccureFrequency.java
@@ -1,4 +1,14 @@
 package info_ammount.calculate;
 
 public class OccureFrequency {
+    public static double charFrequency(String word, char n) {
+        int freq = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == n){
+                freq++;
+            }
+        }
+        return freq/word.trim().length();
+    }
+
 }
